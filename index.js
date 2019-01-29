6d6cb4b578ed00262243d0241128d3d5cd17d563
r.balet <r.balet@ultrastark.ch>
2019-01-29 11:40:45 +0100

    [Add - index package]

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var usBoxShadow = require('./box-shadow/mixin.scss')
+var usGrid = require('./grid/mixin.scss')
+
+module.exports = {
+  usBoxShadow: usBoxShadow,
+  usGrid: usGrid,
+}
